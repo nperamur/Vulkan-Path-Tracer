7497b3c6dc0a581c02c907ca91ce649b4664328e
nperamur <nperamur@gmail.com>
2026-08-04 12:54:26 -0700

    Implemented throughput-based russian roulette

--- scene/SceneManager.cpp
+++ scene/SceneManager.cpp
@@ -52,18 +52,18 @@ SceneManager::SceneManager(Loader& loader, vk::raii::Device& device, vk::raii::P
     // entities.emplace_back(std::move(sponza));
     //
 
-    // WorldObject cornellBox;
-    // cornellBox.entities = std::move(gltfLoader.load("Cornell-Box", loader, device, physicalDevice, mvp, 1.0f, emissiveVertices, lightData));
-    // cornellBox.setRotation(-90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-    // cornellBox.setPosition(glm::vec3(0.0, -1, 0.0));
-    // entities.emplace_back(std::move(cornellBox));
+    WorldObject cornellBox;
+    cornellBox.entities = std::move(gltfLoader.load("Cornell-Box", loader, device, physicalDevice, mvp, 1.0f, emissiveVertices, lightData));
+    cornellBox.setRotation(-90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+    cornellBox.setPosition(glm::vec3(0.0, -1, 0.0));
+    entities.emplace_back(std::move(cornellBox));
 
-    WorldObject cornellBoxSpheres;
-    cornellBoxSpheres.entities = std::move(gltfLoader.load("Cornell-Box-Spheres", loader, device, physicalDevice, mvp, 1.0f, emissiveVertices, lightData));
-    cornellBoxSpheres.setRotation(90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
-    cornellBoxSpheres.setScale(glm::vec3(1.2f, 1.2f, 1.2f));
-    cornellBoxSpheres.setPosition(glm::vec3(0.0, -1, 0.0));
-    entities.emplace_back(std::move(cornellBoxSpheres));
+    // WorldObject cornellBoxSpheres;
+    // cornellBoxSpheres.entities = std::move(gltfLoader.load("Cornell-Box-Spheres", loader, device, physicalDevice, mvp, 1.0f, emissiveVertices, lightData));
+    // cornellBoxSpheres.setRotation(90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+    // cornellBoxSpheres.setScale(glm::vec3(1.2f, 1.2f, 1.2f));
+    // cornellBoxSpheres.setPosition(glm::vec3(0.0, -1, 0.0));
+    // entities.emplace_back(std::move(cornellBoxSpheres));
 
 
     // WorldObject blocks;
@@ -80,7 +80,7 @@ SceneManager::SceneManager(Loader& loader, vk::raii::Device& device, vk::raii::P
     // entities.emplace_back(std::move(cornellBoxMirror));
 
     directionalLight.color = LIGHT_DISABLED;
-    // directionalLight.color = glm::vec4(1.0, 0.95, 0.8, 1.0);
+    //directionalLight.color = glm::vec4(1.0, 0.95, 0.8, 1.0);
 
     for (Entity* entity : getEntities()) {
         entity -> setIndexAddress(entity -> getModel().indexBuffer ? device.getBufferAddress(vk::BufferDeviceAddressInfo{*entity -> getModel().indexBuffer}) : 0);
